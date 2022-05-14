91b2445ee0289b5973dc5fbbcb887eb885fa2dd1
TheVoid2022 <1627017595@qq.com>
2022-05-14 18:15:15 +0800

    gitee commit3

--- GitTest/src/main/java/org/example/Main.java
+++ GitTest/src/main/java/org/example/Main.java
@@ -14,6 +14,5 @@ public class Main {
         System.out.println("push test!");
         System.out.println("pull test!");
         System.out.println("gitee test!");
-        System.out.println("gitee test2!");
     }
 }
